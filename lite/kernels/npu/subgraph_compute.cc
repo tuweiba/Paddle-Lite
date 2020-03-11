e804e93d18a2e595a0c08f93d36cea44cd94b4c6
zhouryGitHub <zhourenyi@baidu.com>
2020-03-11 16:59:14 +0800

    first modify

--- lite/kernels/npu/subgraph_compute.cc
+++ lite/kernels/npu/subgraph_compute.cc
@@ -27,7 +27,7 @@ namespace paddle {
 namespace lite {
 namespace kernels {
 namespace npu {
-
+//I will modify it for ascend310 platform.
 int SubgraphEngine::BuildDeviceProgram() {
   int status = 0;
   // Convert all of ops and their input vars and weights and added into the NPU
